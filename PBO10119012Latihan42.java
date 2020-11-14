c1246803d5a6fb2f5e53bfacaf891176e796894d
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-11-15 01:11:53 +0700

    Add files via upload

---
+++ PBO10119012Latihan42.java
@@ -0,0 +1,25 @@
+
+package pbo10119012latihan42;
+
+
+
+
+/**
+ *  NAMA   : DAMAI SAPUTRA LAOLI
+ *  KELAS  : IF-1
+ *  NIM    : 10119012
+ */
+public class PBO10119012Latihan42 {
+
+    
+    public static void main(String[] args) {
+        private int saldo;
+
+    public Tabungan (int saldo){
+        this.saldo = saldo;
+    }
+
+    public int ambilUang(int jumlah){
+        return this.saldo - jumlah;
+    }
+}
